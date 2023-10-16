a62b8fa15a8798db7ee439c93b6a28a9e52f68ab
SEWARRIO <elzoubisarah@gmail.com>
2023-10-16 11:51:39 +0300

    added comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
+import React from "react";
+import AppRouter from "./pages/AppRouter";
+import { AuthProvider } from "./AuthContext";
+import "./App.css";
 
-import React from 'react';
-import AppRouter from './pages/AppRouter';
-import { AuthProvider } from './AuthContext';
-import './App.css';
-
-
+//comment
 
 const App: React.FC = () => {
-  return (<>
-   
-    <AuthProvider>
-      <div>
-        <AppRouter />
-      </div>
-    </AuthProvider>
+  return (
+    <>
+      <AuthProvider>
+        <div>
+          <AppRouter />
+        </div>
+      </AuthProvider>
     </>
   );
 };
 
 export default App;
-
